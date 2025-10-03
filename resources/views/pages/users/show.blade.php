535d2492e18004648b9129b58519758af5ab73d4
juanmcortez <juanm.cortez@gmail.com>
2025-10-03 17:52:24 -0300

    🚧 User profile on detail view

--- resources/views/pages/users/show.blade.php
+++ resources/views/pages/users/show.blade.php
@@ -1,4 +1,12 @@
 <x-layouts.main>
     <h2>{{ __('User :username', ['username' => $user->username]) }}</h2>
-    <div>{{ $user }}</div>
+    <div>{{ $user->username }}</div>
+    <div>{{ $user->profile->registration_date }}</div>
+    <div>{{ $user->profile->full_name }}</div>
+    <div>{{ $user->profile->birthdate }}</div>
+    <div>{{ $user->profile->email->address }}</div>
+    <div>{{ $user->profile->primary_address->formatted }}</div>
+    <div>{{ $user->profile->primary_phone->formatted }}</div>
+    <div>{{ $user->profile->secondary_address->formatted }}</div>
+    <div>{{ $user->profile->secondary_phone->formatted }}</div>
 </x-layouts.main>
