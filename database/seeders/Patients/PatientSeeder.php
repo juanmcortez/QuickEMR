a173ceb31a65beb2579863b27ace06193ac54906
juanmcortez <juanm.cortez@gmail.com>
2025-10-14 11:19:09 -0300

    🗃️ Insurances subscriber items seeder + Insurances subscriber items foreign

--- database/seeders/Patients/PatientSeeder.php
+++ database/seeders/Patients/PatientSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Encounters\Item;
 use App\Models\Patients\Patient;
 use App\Models\Encounters\Encounter;
+use App\Models\Insurances\Subscriber;
 
 class PatientSeeder extends Seeder
 {
@@ -15,6 +16,21 @@ class PatientSeeder extends Seeder
             ->count(fake()->randomNumber(3))
             ->create()
             ->each(function ($patient) {
+                //
+                Subscriber::factory()
+                    ->create([
+                        'pid_sub' => $patient->pid,
+                        'type' => 'primary',
+                    ]);
+                //
+                if (fake()->randomElement([true, false])) {
+                    Subscriber::factory()
+                        ->create([
+                            'pid_sub' => $patient->pid,
+                            'type' => 'secondary',
+                        ]);
+                }
+                //
                 Encounter::factory()
                     ->count(fake()->randomNumber(1, true))
                     ->create([
