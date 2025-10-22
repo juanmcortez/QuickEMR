8a82181d12aab671cb474f2967503126f9e85c92
juanmcortez <juanm.cortez@gmail.com>
2025-10-22 17:34:47 -0300

    🚧 Code types enum modifications.

--- app/Enum/CodeType.php
+++ app/Enum/CodeType.php
@@ -18,16 +18,48 @@ enum CodeType: string
     case SNOMEDCT = 'snomedct';
     case SNOMEDPR = 'snomedpr';
 
-    // Optional: Helper method to get a random type
-    public static function random(): self
+    // Helper method to get a random type
+    public static function random(array $procedures): self
     {
-        $cases = self::cases();
+        $cases = (empty($procedures)) ? self::cases() : $procedures;
         return $cases[array_rand($cases)];
     }
 
-    // Optional: Get all values as array
+    // Get a random procedure code type (CPT4, HCPCS, ANES, or CVX)
+    public static function randomProcedureCode(): self
+    {
+        $procedureCodes = [
+            self::CPT4,
+            self::HCPCS,
+            self::ANES,
+            self::CVX,
+        ];
+        return self::random($procedureCodes);
+    }
+
+    // Get all values as array
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
+    }
+
+    // Get human-readable label
+    public function label(): string
+    {
+        return match ($this) {
+            self::ANES => 'Anesthesia',
+            self::CPT4 => 'CPT-4',
+            self::CPTII => 'CPT-II',
+            self::CVX => 'CVX (Vaccines)',
+            self::DSMIV => 'DSM-IV',
+            self::HCPCS => 'HCPCS',
+            self::ICD10 => 'ICD-10',
+            self::ICD10PCS => 'ICD-10-PCS',
+            self::ICD9 => 'ICD-9',
+            self::ICD9SG => 'ICD-9-SG',
+            self::SNOMED => 'SNOMED',
+            self::SNOMEDCT => 'SNOMED CT',
+            self::SNOMEDPR => 'SNOMED PR',
+        };
     }
 }
