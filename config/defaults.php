ca6fed53f81a8e64eed52dc8b9978eb84a923f0b
juanmcortez <juanm.cortez@gmail.com>
2025-10-24 15:00:52 -0300

    🔧 New default configurations file.

---
+++ config/defaults.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Split Character
+    |--------------------------------------------------------------------------
+    |
+    | This value is the separator character that is going to be used
+    | on the urls slug combination in the Custom Code models.
+    |
+    */
+
+    'slug_split' => env('DEF_SLUG_SPLIT', ':'),
+
+];
